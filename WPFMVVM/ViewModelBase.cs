f7504b7dfb9bade115be707a9e760c1ef40080d0
Dirk Kramer <46933603+rapidengineering@users.noreply.github.com>
2022-09-27 10:34:35 +0200

    Add project files.

---
+++ WPFMVVM/ViewModelBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Windows;
+
+namespace WPFMVVM
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public void CloseWindowWithThisViewmodel()
+        {
+            foreach (Window item in Application.Current.Windows)
+            {
+                if (item.DataContext == this) item.Close();
+            }
+        }
+    }
+}
